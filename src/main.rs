37560a668d43632c9be4bf6eda3889edb9f01881
VoltagedDebunked <rusindanilo@gmail.com>
2024-10-13 11:01:51 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use colored::Colorize;
+use deno_core::error::AnyError;
+use std::env;
+use std::rc::Rc;
+
+pub struct JsExecutor {
+    js_runtime: deno_core::JsRuntime,
+}
+
+impl JsExecutor {
+    pub fn new() -> Result<Self, AnyError> {
+        let js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+            module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+            ..Default::default()
+        });
+        Ok(JsExecutor { js_runtime })
+    }
+
+    pub async fn run_js(&mut self, file_path: &str) -> Result<(), AnyError> {
+        let main_module = deno_core::resolve_path(file_path, &env::current_dir()?)?;
+        let mod_id: usize = self.js_runtime.load_main_es_module(&main_module).await?;
+        let result = self.js_runtime.mod_evaluate(mod_id);
+        self.js_runtime.run_event_loop(Default::default()).await?;
+        result.await
+    }
+}
+
+fn main() {
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("{}", "Usage: zen <path_to_js_file>".yellow().bold());
+        return;
+    }
+
+    let file_path = &args[1];
+    let mut executor = JsExecutor::new().unwrap();
+    if let Err(error) = runtime.block_on(executor.run_js(file_path)) {
+        eprintln!("error: {}", error);
+    }
+}
